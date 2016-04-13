1467b9fd2b0d9183288684d5cd5c0109f22b8328
cnlohr <lohr85@gmail.com>
2016-04-13 01:42:05 -0400

    Whoops forgot to fix the searchnet project for new ping technique

--- searchnet.c
+++ searchnet.c
@@ -84,7 +84,8 @@ int main( int argc, char ** argv )
 		send_id[2] = (cur>>8)&0xff;
 		send_id[3] = (cur)&0xff;
 //		printf( "Pinging: %s\n", dispip );
-		do_pinger( dispip, -1 );
+		pingperiod = -1;
+		do_pinger( dispip );
 
 		OGUSleep( (int)(speed * 1000000) );
 	}
