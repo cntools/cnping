94a6eba48c6e27cc0a243b7515b66a76ba63054c
CNLohr <charles@cnlohr.com>
2017-06-21 10:48:49 -0400

    Update ping.h

--- ping.h
+++ ping.h
@@ -27,7 +27,11 @@ void ping_setup();
 #ifdef WIN32
 
 extern char errbuffer[1024];
+#ifndef _MSC_VER
 #define ERRM(x...) { sprintf( errbuffer, x ); MessageBox( 0, errbuffer, "cnping", 0 ); }
+#else
+#define ERRM(...) { sprintf( errbuffer, __VA_ARGS__ ); MessageBox( 0, errbuffer, "cnping", 0 ); }
+#endif
 
 #else
 
