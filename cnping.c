82fadd130e987fa4369a29bd36d5953606e88e17
cnlohr <lohr85@gmail.com>
2019-09-27 01:54:52 -0400

    Correct version number to a meaningful number.

--- cnping.c
+++ cnping.c
@@ -1,6 +1,6 @@
 //Copyright (c) 2011-2019 <>< Charles Lohr - Under the MIT/x11 or NewBSD License you choose.
 
-#define VERSION "1.0-pre"
+#define VERSION "1.0b1"
 
 #include <stdio.h>
 #include <stdlib.h>
