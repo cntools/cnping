a98be46f123fc0503a32367ed6a0c7668659612c
David Auer <dreua@posteo.de>
2019-10-04 20:03:04 +0200

    Bump version to 1.0.0

--- cnping.c
+++ cnping.c
@@ -1,6 +1,6 @@
 //Copyright (c) 2011-2019 <>< Charles Lohr - Under the MIT/x11 or NewBSD License you choose.
 
-#define VERSION "1.0b3"
+#define VERSION "1.0.0"
 
 #include <stdio.h>
 #include <stdlib.h>
